ab98492c0193e4b56a1a7e668a3eadd9057bc705
Saranya Vatti <saranyavatti@ufl.edu>
2017-11-05 23:51:39 -0500

    Update extra-sweet.java

--- Challenge/extra-sweet.java
+++ Challenge/extra-sweet.java
@@ -28,6 +28,8 @@ public class Solution {
     }
 
     private static Node search(BigInteger l, BigInteger r, int start, int end) {
+        // Binary search to find where the asked sweet range falls
+        // Input is always valid meaning there will always be a node
         Node startNode = alist.get(start);
         if(start >= end) {
             globalIndex = start;
@@ -54,9 +56,12 @@ public class Solution {
         Scanner in = new Scanner(System.in);
         BigInteger n = in.nextBigInteger();
         int s = in.nextInt();
+        // initially, there will just be one node with value [0,n], meaning everything is available
+        // at any given time, our arraylist is going to look like: [0,5]->[8,10]->[11,11]->[13,40] something like this
         Node head = new Node(BigInteger.ZERO, n.subtract(BigInteger.ONE));
         alist.add(head);
         for(int a0 = 0; a0 < s; a0++){
+            // Input is alwas valid .. so whenever the sweet is asked the range should be entirely in one of the nodes
             BigInteger l = in.nextBigInteger();
             BigInteger r = in.nextBigInteger();
             BigInteger ans=sum(l, r);
@@ -71,6 +76,7 @@ public class Solution {
             Node rightNode = null;
             if(currIndex-1 >= 0 && currIndex-1 <= alist.size()-1) leftNode = alist.get(currIndex-1);
             if(currIndex+1 >= 0 && currIndex+1 <= alist.size()-1) rightNode = alist.get(currIndex+1);
+            // case1 - either the left end of a node is same as the one they ask
             if(curr.left.equals(l)) {
                 if(leftNode != null) {
                     ans = ans.add(leftNode.right);
@@ -88,6 +94,7 @@ public class Solution {
                 }
                 ans = ans.add(lMinusOne);
             }
+            // case2 - either the right end of a node is same as the one they ask
             if(curr.right.equals(r)) {
                 if(rightNode != null) {
                     ans = ans.add(rightNode.left);
@@ -108,6 +115,9 @@ public class Solution {
                 alist.remove(currIndex);
                 currIndex--;
             }
+            // whenever something is taken, then the node will split into two.. if 2 to 6 is taken, then it is split to [0,0] and [8,n]
+            // because along with 2 and 6, 1 and 7 are also given away
+            // case 3 - both ends are completely within a node
             if(curr.left.compareTo(lMinusOne)<0 && rPlusOne.compareTo(curr.right)<0) {
                 alist.add(currIndex+1, new Node(rPlusTwo, curr.right));
                 curr.right = lMinusTwo;
